5b1ff14a6d52f365243523709c485e1e8157a43e
Dung Nguyen <tiendungnguyen.4476@gmail.com>
2024-08-07 11:37:49 +0700

    fix port

--- note.bat
+++ note.bat
@@ -32,7 +32,7 @@ docker run --network="host" test-coworking-analytics
 
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 536577976564.dkr.ecr.us-east-1.amazonaws.com
-docker build -t coworking .
+docker build -t   .
 docker tag coworking:latest 536577976564.dkr.ecr.us-east-1.amazonaws.com/coworking:latest
 docker push 536577976564.dkr.ecr.us-east-1.amazonaws.com/coworking:latest
 
